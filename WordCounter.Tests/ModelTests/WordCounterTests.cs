6270355b2b8528fb720d0e75085fdbd2db355ff4
Matt Stroud <lixingke06@gmail.com>
2020-05-15 08:50:51 -0700

    add test for Constructor storing input word

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -6,6 +6,15 @@ namespace WordCounter.Tests
   [TestClass]
   public class RepeatCounterTests
   {
+    [TestMethod]
+    public void Constructor_StoresWord_Word()
+    {
+      string newWord = "the";
+      RepeatCounter wordCounter = new RepeatCounter(newWord);
 
+      string result = wordCounter.Word;
+
+      Assert.AreEqual(result, newWord);
+    }
   }
 }

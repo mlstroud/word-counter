44865ccab6dbedd57a8a0c4fbeaadc9e859efce2
Matt Stroud <lixingke06@gmail.com>
2020-05-15 08:51:20 -0700

    update Constructor to store input word

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,6 +2,11 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    public string Word { get; set; }
 
+    public RepeatCounter(string word)
+    {
+      Word = word;
+    }
   }
 }
